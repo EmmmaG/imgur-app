f29896915a46d923c18dd0f41d7dfec2586d5972
EmmmaG <gongyuan1987@gmail.com>
2017-06-16 10:58:18 +1000

    second commit

--- src/app/display/display.service.ts
+++ src/app/display/display.service.ts
@@ -7,12 +7,16 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class DisplayService {
 
-    private displayUrl = 'https://api.imgur.com/3/gallery/hot/viral/0';
+    private displayUrl = 'https://api.imgur.com/3/account/EmmmaG/images/0';
 
     constructor(private http: Http) {}
 
     getdata(): Promise<any[]> {
-        return this.http.get(this.displayUrl)
+        let accessToken = '1914e1723a1084bbd906592fb7f79f253f94a9e0';
+        let headers = new Headers({'Authorization': 'Bearer '+accessToken});
+        let options = new RequestOptions({ headers: headers });
+
+        return this.http.get(this.displayUrl, options)
                         .toPromise()
                         .then(this.extractData)
                         .catch(this.handleError);
