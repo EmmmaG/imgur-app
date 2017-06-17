26d135844f2f75376c9513ad06dec92bb5b6faf3
EmmmaG <gongyuan1987@gmail.com>
2017-06-17 10:05:18 +1000

    3rd commit with upload function and style changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,23 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { displayComponent } from './display/display.component';
+import { uploadComponent } from './upload/upload.component';
 
 import { DisplayService } from './display/display.service';
+import { UploadService } from './upload/upload.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    displayComponent
+    displayComponent,
+    uploadComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [DisplayService],
+  providers: [DisplayService, UploadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
